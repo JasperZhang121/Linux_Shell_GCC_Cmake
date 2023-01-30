1e808b61f1382c07f5881b9ec22eef9d045f65a7
Hao <jasperzhang121@gmail.com>
2023-01-31 09:53:46 +1100

    cmake

---
+++ Cpp/p1/build/CMakeFiles/hello_cmake.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_cmake.dir/hw.cpp.o"
+  "CMakeFiles/hello_cmake.dir/hw.cpp.o.d"
+  "hello_cmake"
+  "hello_cmake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_cmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
